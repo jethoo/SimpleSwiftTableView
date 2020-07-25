b049c8287899022e18b38da4b84db9bf5c2c5527
jethoo <jeewanbhusal@gmail.com>
2020-07-24 22:42:16 -0400

    Initial Commit

---
+++ Jeewan-Bhusal_COMP2125-Sec001_Lab04_Version2/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  Jeewan-Bhusal_COMP2125-Sec001_Lab04_Version2
+//
+//  Created by jeewan bhusal on 24.7.2020.
+//  Copyright © 2020 jeewan bhusal. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ Jeewan-Bhusal_COMP2125-Sec001_Lab04_Version2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jeewan-Bhusal_COMP2125-Sec001_Lab04_Version2
+//
+//  Created by jeewan bhusal on 24.7.2020.
+//  Copyright © 2020 jeewan bhusal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
